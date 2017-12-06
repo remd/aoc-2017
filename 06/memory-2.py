46415552b8268f55e48d1524647ed91917cfee06
Eric Dostie <ericmdostie@gmail.com>
2017-12-06 00:28:32 -0500

    Add day 6, part 2

---
+++ 06/memory-2.py
@@ -0,0 +1,42 @@
+def checksum(memory):
+    return "".join([str(mem) for mem in memory])
+
+def getIndexOfMaxBank(memory):
+    largest = 0
+    for num in memory:
+        if num > largest:
+            largest = num
+    return memory.index(largest)
+
+def reallocate(memory, i):
+    num = memory[i]
+    memory[i] = 0
+    while num > 0:
+        i += 1
+        if i == len(memory):
+            i = 0
+        memory[i] = memory[i] + 1
+        num -= 1
+
+f = open("input.txt", "r")
+memory = [int(num) for num in f.read().split()]
+count = 0
+checksums = dict()
+loopFlag = False
+while True:
+    chk = checksum(memory)
+    if chk in checksums:
+        if loopFlag:
+            break
+        else:
+            loopFlag = True
+            count = 0
+            checksums = dict()
+            checksums[chk] = 1
+            reallocate(memory, getIndexOfMaxBank(memory))
+    else:
+        checksums[chk] = 1
+        reallocate(memory, getIndexOfMaxBank(memory))
+    count += 1
+
+print count
